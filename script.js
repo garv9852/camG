6b845fccbe24d2a1666dfd77449df20d908f3245
Garv kapoor <Garvk@LAPTOP-DEDK2DPV.(none)>
2021-10-23 01:50:57 +0530

    v2

--- script.js
+++ script.js
@@ -9,13 +9,32 @@ let recordbtn=document.querySelector(".record-btn");
 let capturebtncont=document.querySelector(".capture-container");
 let capturebtn=document.querySelector(".capture-btn");
 let recordCheck=false
+let chunks;
 
 navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
     video.srcObject=stream;
     mediarecord=new MediaRecorder(stream);
+    mediarecord.addEventListener("start",function(e){
+        chunks=[];
+    })
+    mediarecord.addEventListener("dataavailable",function(e){
+        chunks.push(e.data);
+    })
+    mediarecord.addEventListener("stop",function(e){
+        let nam=prompt("Video Name:")
+        let blob=new Blob(chunks,{type:`video/mp4`});
+        let videoUrl=URL.createObjectURL(blob);
+        let a=document.createElement('a');
+        a.href=videoUrl;
+        a.download=`${nam}.mp4`;
+        a.click();
+    })
 })
 recordbtncont.addEventListener('click',function(){
-    if(!mediarecord) return;
+    if(!mediarecord) {
+        alert("Your Camera is Disabled");
+        return;
+    }
     if(!recordCheck)
     {
         mediarecord.start();
@@ -27,4 +46,5 @@ recordbtncont.addEventListener('click',function(){
         recordbtn.classList.remove("scale-record");
     }
 })
+
 
